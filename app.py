03ef587ddb2b9a81ca98f0372a4a3b3a1b1a3814
FelixAlvarado <felixalvarado653@gmail.com>
2020-08-21 18:19:21 -0700

    removed comment

--- app.py
+++ app.py
@@ -2,4 +2,3 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-# mySQL is installed. need to follow docs for database https://dev.mysql.com/doc/connector-python/en/connector-python-introduction.html
