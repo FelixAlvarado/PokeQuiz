9c90f611798c59a87e59e07450bff5b038cf8506
FelixAlvarado <felixalvarado653@gmail.com>
2020-09-11 15:45:34 -0700

    finished window css functionality

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -3,9 +3,14 @@ import '../style/nav.css'
 
 export default function Nav() {
 
+  function reload(e){
+    e.preventDefault()
+    window.location.reload()
+  }
+
   return (
     <div className="nav-bar">
-      <div className='logo' ></div>
+      <div onClick={(e) => reload(e)} className='logo' ></div>
       <button className='create-quiz-button'>Create Quiz</button>
     </div>
   );    
