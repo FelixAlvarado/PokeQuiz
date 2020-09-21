67a45e8d7656321c5d8dfe86f57375f5f7ae972a
FelixAlvarado <felixalvarado653@gmail.com>
2020-09-21 11:36:37 -0700

    modified top portion of quiz page

--- frontend/src/components/QuizPage.js
+++ frontend/src/components/QuizPage.js
@@ -1,7 +1,11 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { useParams } from "react-router-dom"
 import { getQuiz  } from '../app/quizesSlice.js'
 import { useSelector, useDispatch } from 'react-redux';
+import '../style/quizpage.css'
+import {pokePicture} from '../utility/util'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCopy, faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 
 export default function QuizPage() {
@@ -9,24 +13,47 @@ export default function QuizPage() {
   let { id } = useParams();
   const quiz = useSelector(state => state.quizes[`${id}`])
   const onLoad = useRef(true)
+  const [picture, setPicture] = useState('')
   console.log('here is the quiz', quiz)
+  const title = quiz ? quiz.title : ''
+
+
+  function handleImage(){
+    if (picture.length > 0){
+      return(
+        <img alt="pokemon" src={picture} className="page-picture"/>
+      )
+    } else {
+      return(
+        <div className="page-picture"></div>
+      )
+    }
+  }
 
 
 useEffect(() => {
   if(onLoad.current){
     dispatch(getQuiz(id))
+    setPicture(pokePicture())
     onLoad.current = false;
   }
 });
 
   return (
-    <div>
-      <p>here is the id: {id}</p>
-      <p>here is the id: {id}</p>
-      <p>here is the id: {id}</p>
-      <p>here is the id: {id}</p>
-      <p>here is the id: {id}</p>
-      <p>here is the id: {id}</p>
+    <div className="page-holder">
+      <div className="page-top-section">
+        {handleImage()}
+          <div className="page-top-right">
+            <div className="page-title"><div>{title}</div></div>
+              <div className="link-button-holder">
+              <button className="take-quiz-button">Take Quiz</button>
+              <div className="page-link-holder">
+                <input defaultValue={window.location.href}/>
+                <FontAwesomeIcon className="copy-icon" icon={faCopy} size="lg" />
+              </div>
+            </div>
+          </div>
+      </div>
     </div>
   );    
 }
