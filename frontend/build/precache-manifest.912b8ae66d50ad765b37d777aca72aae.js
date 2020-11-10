2528559d8d70998c8a33aacfae701b417ba03afd
FelixAlvarado <felixalvarado653@gmail.com>
2020-11-09 17:15:57 -0800

    ran build

---
+++ frontend/build/precache-manifest.912b8ae66d50ad765b37d777aca72aae.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "052a0360497d5ba0dad4cd0081efb36a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68ee9cf0ecaa6dbf42a5",
+    "url": "/static/css/main.a6bba9f5.chunk.css"
+  },
+  {
+    "revision": "2b5134300643163802b6",
+    "url": "/static/js/2.3624f66c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3624f66c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68ee9cf0ecaa6dbf42a5",
+    "url": "/static/js/main.53eb13fe.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "b73af9e7bdeafccdbaf333c67261fee7",
+    "url": "/static/media/background.b73af9e7.jpg"
+  },
+  {
+    "revision": "fbfddf025063ec84dc97a6ae4a4a268f",
+    "url": "/static/media/logo.fbfddf02.jpg"
+  },
+  {
+    "revision": "fdc883027c06157698d89501b83212f2",
+    "url": "/static/media/pokeball2.fdc88302.png"
+  }
+]);
